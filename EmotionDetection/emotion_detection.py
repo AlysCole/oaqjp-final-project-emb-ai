dcbd9379f29d5c31564b594980d87578d8c7db18
AlysCole <hello@alyssanqa.com>
2025-04-30 09:09:15 -0400

    Add error handling, fix static code analysis errors

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,18 +6,29 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     obj = { "raw_document": { "text": text_to_analyse } }
     res = requests.post(url, json = obj, headers = header)
-    parsed = json.loads(res.text)
-    
-    emotions = parsed['emotionPredictions'][0]['emotion']
 
-    highest = { "emotion": "", "value": 0 }
-    for emotion in emotions:
-        if emotions[emotion] > highest['value']:
-            highest = {
-                "emotion": emotion,
-                "value": emotions[emotion]
-            }
+    if res.status_code == 200:
+        parsed = json.loads(res.text)
+        
+        emotions = parsed['emotionPredictions'][0]['emotion']
 
-    emotions['dominant_emotion'] = highest['emotion']
+        highest = { "emotion": "", "value": 0 }
+        for emotion in emotions:
+            if emotions[emotion] > highest['value']:
+                highest = {
+                    "emotion": emotion,
+                    "value": emotions[emotion]
+                }
+
+        emotions['dominant_emotion'] = highest['emotion']
+    elif res.status_code == 400:
+        return {
+            "anger": None, 
+            "disgust": None, 
+            "fear": None, 
+            "joy": None, 
+            "sadness": None, 
+            "dominant_emotion": None
+        }
 
     return emotions
--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+''' Import flask modules and emotion detection package
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,13 +7,22 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    ''' Route to analyze text and return emotion prediction
+    '''
     text_to_analyse = request.args.get('textToAnalyze')
     res = emotion_detector(text_to_analyse)
-    formatted = f"For the given statement, the system response is 'anger': {res['anger']}, 'disgust': {res['disgust']}, 'fear': {res['fear']}, 'joy': {res['joy']}, 'sadness': {res['sadness']}. The dominant emotion is <b>{res['dominant_emotion']}</b>"
+    if res['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
+    formatted = f"For the given statement, the system response is 'anger': \
+    {res['anger']}, 'disgust': {res['disgust']}, 'fear': {res['fear']}, 'joy': {res['joy']}, \
+    'sadness': {res['sadness']}. The dominant emotion is <b>{res['dominant_emotion']}</b>"
     return formatted
 
 @app.route("/")
 def render_index_page():
+    ''' Route to return templated page
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
